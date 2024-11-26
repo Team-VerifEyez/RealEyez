1813a466ad18e3eea407014a393e2dc695ca941d
Terry Woolard <terrywoolard@Terrys-MBP.attlocal.net>
2024-11-26 17:06:13 -0500

    Terraform directory structure

--- RealVsAI/settings.py
+++ RealVsAI/settings.py
@@ -137,6 +137,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dinnynemade@gmail.com'  # Your Gmail address
+EMAIL_HOST_USER = 'tjwkura5@gmail.com'  # Your Gmail address
 EMAIL_HOST_PASSWORD = '123456'  # Use App Password if 2FA is enabled
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
---
+++ Terraform/Dev/modules/network(VPC)/outputs.tf
@@ -0,0 +1,28 @@
+output "vpc_id" {
+  value = ""
+}
+
+output "public_subnet_id_1" {
+  value = ""
+}
+
+output "public_subnet_id_2" {
+  value = ""
+}
+
+output "private_subnet_id_1_az1" {
+  value = ""
+}
+
+output "private_subnet_id_1_az2" {
+  value = ""
+}
+
+output "private_subnet_id_2_az1" {
+  value = ""
+}
+
+output "private_subnet_id_2_az2" {
+  value = ""
+}
+
