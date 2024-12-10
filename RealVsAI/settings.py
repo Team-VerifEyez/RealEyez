f1f683f7234e49c257f3d43a061341a698722dc8
Uzo Bolarinwa <uzobolar@gmail.com>
2024-12-09 20:19:37 -0600

    Update settings.py
    Removed Debug

--- RealVsAI/settings.py
+++ RealVsAI/settings.py
@@ -25,7 +25,7 @@ TEMP_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ['DJANGO_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
 ALLOWED_HOSTS = ['*']
 
