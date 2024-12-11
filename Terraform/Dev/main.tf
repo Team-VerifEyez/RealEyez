cb73191ad31beb9799b89029624d6081b63794ce
acurwen <yanwen2627@gmail.com>
2024-12-11 02:23:24 +0000

    create dbucket for testing

--- Terraform/Dev/main.tf
+++ Terraform/Dev/main.tf
@@ -6,10 +6,9 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-terraform-state"
+    bucket         = "realeyez-state-bucket"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
     encrypt        = true
   }
 }
